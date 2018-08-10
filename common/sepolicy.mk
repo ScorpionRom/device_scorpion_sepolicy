5ba89c7ff080cfb0a0b842e4e745ee7f9abd2c3e
blkscorp33 <don.berzley@gmail.com>
2018-08-09 20:33:45 -0400

    Initial Scorpion rebase
    Change-Id: I95dfc03cf60cb566ec7ced9300ce64e79b64e950

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -4,10 +4,10 @@
 #
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/lineage/sepolicy/common/public
+    device/scorpion/sepolicy/common/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/common/private
+    device/scorpion/sepolicy/common/private
 
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/vendor
+    device/scorpion/sepolicy/common/vendor
--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -4,8 +4,8 @@
 #
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/qcom/private
+    device/scorpion/sepolicy/qcom/private
 
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/qcom/common \
-    device/lineage/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
+    device/scorpion/sepolicy/qcom/common \
+    device/scorpion/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
